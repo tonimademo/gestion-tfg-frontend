b17a6a73ad840daba844c2b18c1f0c9c7380d040
tonimademo <tonimademo@gmail.com>
2016-01-16 13:20:03 +0100

    master primer request a baackend

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,26 @@
 function inicio(response) {
+  //Load the request module
+  var request = require('request');
   console.log("Llamada a 'inicio'");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Inicio");
-  response.end();
+  //response.writeHead(200, {"Content-Type": "text/html"});
+  //response.write("Hola Inicio");
+  //response.end();
+  request('http://127.0.0.1:8000/ejemplo', function (error, resp, body) {
+    //Check for error
+    if(error){
+        return console.log('Error:', error);
+    }
+
+    //Check for right status code
+    if(resp.statusCode !== 200){
+        return console.log('Invalid Status Code Returned:', resp.statusCode);
+    }
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+
+  });
+
 }
 
 function subir(response) {
