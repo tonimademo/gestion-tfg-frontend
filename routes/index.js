d38eb064d8060a02b96f8e2c7236b64104592e49
tonimademo <tonimademo@gmail.com>
2016-01-18 21:20:21 +0100

    master llamada a WS de back pasando parametros

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
-
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
 
   var request = require('request');
+
   console.log("Llamada a 'inicio'");
   //response.writeHead(200, {"Content-Type": "text/html"});
   //response.write("Hola Inicio");
   //response.end();
-  request('http://127.0.0.1:8000/ejemplo', function (error, response, body) {
+  request({
+    url:'http://127.0.0.1:8000/insert_tfg', //URL to hit
+    qs: {tfg_titulo: 'titulo'}, //Query string data
+    method: 'GET', //TODO:El metodo POST devuelve un 500 sin llegar al back, revisar
+    headers: { //We can define headers too
+        'Content-Type': 'MyContentType',
+        'Custom-Header': 'Custom Value'
+    }
+  },function (error, response, body) {
     //Check for error
     if(error){
         return console.log('Error:', error);
